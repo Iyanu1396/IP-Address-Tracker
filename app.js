2d0836e243567e72c3ab2d673476035e8a269cfe
Iyanu Oluwa <iyanu1396@gmail.com>
2023-12-16 03:36:52 +0100

    commit

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ navigator.geolocation.getCurrentPosition(
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     console.log(latitude, longitude);
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map("map", {
+      zoomControl: false,
+    }).setView([latitude, longitude], 13);
 
-L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-L.marker([latitude , longitude]).addTo(map)
-    .openPopup();
+    L.marker([latitude, longitude]).addTo(map).openPopup();
   },
   function () {
     alert("Could not get your position");
