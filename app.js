069153d5257373f04f55cc06d481641fb20ae74e
Iyanu Oluwa <iyanu1396@gmail.com>
2023-12-16 16:11:06 +0100

    commit

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
 "use strict";
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
-    console.log(latitude, longitude);
-    var map = L.map("map", {
-      zoomControl: false,
-    }).setView([latitude, longitude], 13);
+const headerEl = document.querySelector("header");
 
-    L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+const renderIPData = (ip, timeZone) => {
 
-    L.marker([latitude, longitude]).addTo(map).openPopup();
-  },
-  function () {
-    alert("Could not get your position");
-  }
-);
+  const html = ` 
+    <div class="ip-data container">
+      <small> IP ADDRESS </small>
+      <h3>${ip.ip}</h3>
+      <small> LOCATION </small>
+      <h3>${ip.location.region}, ${ip.location.country} ${ip.location.postalCode}</h3>
+      <small> TIMEEZONE </small>
+      <h3>${timeZone} ${ip.location.timezone}</h3>
+      <small> ISP </small>
+      <h3>${ip.isp}</h3>
+  </div>
+`;
+
+  headerEl.innerHTML += html;
+};
+
+const renderMap = () => {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      var map = L.map("map", {
+        zoomControl: false,
+      }).setView([latitude, longitude], 13);
+
+      L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([latitude, longitude]).addTo(map).openPopup();
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
+};
+
+const getIPData = async () => {
+  const res1 = fetch(
+    "https://geo.ipify.org/api/v2/country,city?apiKey=at_jgwwq3DRDt1jZmGajaAzxoYOKUFtc"
+  ).then((res) => res.json());
+
+  const res2 = fetch(
+    "https://timezoneapi.io/api/ip/?token=amfhcIRUCzfROLcHjmAM"
+  ).then((res) => res.json());
+
+  const [ipData, timeZoneDate] = await Promise.all([res1, res2]);
+
+  const timeZone = String(timeZoneDate.data.datetime.offset_tzfull)
+    .split(" ")
+    .map((word) => word.charAt(0))
+    .join("");
+
+  renderIPData(ipData, timeZone);
+  console.log(ipData);
+  renderMap();
+};
+
+getIPData()
