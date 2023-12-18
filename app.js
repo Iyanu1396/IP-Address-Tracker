578dda40a8e3dcce984c5ea0305bc4632fc7fa1d
Iyanu Oluwa <iyanu1396@gmail.com>
2023-12-19 00:18:09 +0100

    final commit for now i guess.....

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const ipContainer = document.querySelector(".ip-data");
 const inputEl = document.querySelector(".input");
 const searchBtn = document.querySelector(".icon-arrow");
 const form = document.querySelector(".form");
+const map = document.querySelector("#map1");
+inputEl.value = "";
 
-// Render Ip data on UI
+// Render Error
+const renderError = function () {
+  ipContainer.innerHTML = "";
+  const html = `<h3>Couldn't display IP info kindly check your internet connection or try again later 😔</h3> `;
+  ipContainer.innerHTML = html;
+};
+
+// Render Ip data on UI on page load
 const renderIPData = (ip, timeZone) => {
-  ipContainer.insertAdjacentHTML = "";
-  // inputEl.value = ip.ip;
+  ipContainer.innerHTML = "";
   const html = ` 
   <div>
   <small> IP ADDRESS </small>
@@ -31,32 +39,52 @@ const renderIPData = (ip, timeZone) => {
   ipContainer.innerHTML = html;
 };
 
-// Display Map
-var customIcon = L.icon({
-  iconUrl: "images/icon-location.svg",
-  iconSize: [50, 60],
-  iconAnchor: [19, 5],
-  popupAnchor: [0, -38],
-});
+// Render Ip data on UI on search
+const renderIPDataSearch = (ip, timeZone) => {
+  ipContainer.insertAdjacentHTML = "";
+  // inputEl.value = ip.ip;
+  const html = ` 
+  <div>
+  <small> IP ADDRESS </small>
+  <h3>${ip.ip}</h3>
+</div>
+<div class="divider">
+<small> LOCATION </small>
+<h3>${ip.location.region}, ${ip.location.country} ${ip.location.postalCode}</h3>
+</div>
+<div class="divider">
+<small> ISP </small>
+<h3>${ip.isp}</h3>
+</div>
+`;
 
+  ipContainer.innerHTML = html;
+};
+
+// Display Map on page load
+let map1;
 const renderMap = () => {
+  var customIcon = L.icon({
+    iconUrl: "images/icon-location.svg",
+    iconSize: [50, 60],
+    iconAnchor: [19, 5],
+    popupAnchor: [0, -38],
+  });
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      var map = L.map("map", {
+      map1 = L.map("map1", {
         zoomControl: false,
-        // scrollWheelZoom: "center",
-        // dragging: false,
       }).setView([latitude, longitude], 15);
 
       L.tileLayer(
         "https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
         {}
-      ).addTo(map);
+      ).addTo(map1);
 
       var marker = L.marker([latitude, longitude], { icon: customIcon }).addTo(
-        map
+        map1
       );
     },
     function () {
@@ -65,11 +93,40 @@ const renderMap = () => {
   );
 };
 
+// Display Map on search
+
+let map2;
+const renderMapSearch = (lat, lng) => {
+  map.style.display = "none";
+  if (map1) {
+    map1.remove();
+  }
+
+  var customIcon = L.icon({
+    iconUrl: "images/icon-location.svg",
+    iconSize: [50, 60],
+    iconAnchor: [19, 5],
+    popupAnchor: [0, -38],
+  });
+
+  map2 = L.map("map2", {
+    zoomControl: false,
+  }).setView([lat, lng], 15);
+
+  L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {}).addTo(
+    map2
+  );
+
+  var marker = L.marker([lat, lng], { icon: customIcon }).addTo(map2);
+
+  console.log(map1);
+};
+
 // Get IP data on page load
 const getIPData = async () => {
   try {
     const res1 = fetch(
-      "https://geo.ipify.org/api/v2/country,city?apiKey=at_4HJeO7HnHJvDPvyC2W5SMVFqqgEMU"
+      "https://geo.ipify.org/api/v2/country,city?apiKey=at_CCdKhbjkOlcSEUlc8gAfZLbnA07gQ"
     ).then((res) => res.json());
 
     const res2 = fetch(
@@ -83,36 +140,37 @@ const getIPData = async () => {
       .map((word) => word.charAt(0))
       .join("");
 
-    renderIPData(ipData, timeZone);
     renderMap();
-
+    renderIPData(ipData, timeZone);
     return timeZone;
   } catch (err) {
-    console.log(err.message);
+    renderError();
   }
 };
 getIPData();
 
 // Search for IP address
-const fetchIP = async function (ip) {
+const searchIP = async function (ip) {
   try {
     const res = await fetch(
-      `https://geo.ipify.org/api/v2/country,city?apiKey=at_4HJeO7HnHJvDPvyC2W5SMVFqqgEMU&ipAddress=${ip}`
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_CCdKhbjkOlcSEUlc8gAfZLbnA07gQ&ipAddress=${ip}`
     );
     const ipData = await res.json();
     const timeZone = await getIPData();
-    renderIPData(ipData, timeZone);
+    renderIPDataSearch(ipData, timeZone);
+    renderMapSearch(ipData.location.lat, ipData.location.lng);
   } catch (err) {
-    console.log(err.message);
+    renderError();
   }
 };
 
+// Search button event listener
 searchBtn.addEventListener("click", function () {
-  fetchIP(inputEl.value);
+  searchIP(inputEl.value);
 });
 
 // Prevent Default on input
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  fetchIP(inputEl.value);
+  searchIP(inputEl.value);
 });
