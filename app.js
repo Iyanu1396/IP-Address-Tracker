dcedfb1dfb35924ad9283841ca02f3c57b06f6ee
Iyanu Oluwa <iyanu1396@gmail.com>
2023-12-18 15:43:51 +0100

    commit

--- app.js
+++ app.js
@@ -2,50 +2,58 @@
 
 const ipContainer = document.querySelector(".ip-data");
 const inputEl = document.querySelector(".input");
-const submitBtn = document.querySelector(".icon-arrow");
+const searchBtn = document.querySelector(".icon-arrow");
 const form = document.querySelector(".form");
 
-// Render Ip infos on UI
+// Render Ip data on UI
 const renderIPData = (ip, timeZone) => {
-  inputEl.value = ip.ip;
+  ipContainer.insertAdjacentHTML = "";
+  // inputEl.value = ip.ip;
   const html = ` 
-    
-      <small> IP ADDRESS </small>
-      <h3>${ip.ip}</h3>
-      <small> LOCATION </small>
-      <h3>${ip.location.region}, ${ip.location.country} ${ip.location.postalCode}</h3>
-      <small> TIMEEZONE </small>
-      <h3>${timeZone} ${ip.location.timezone}</h3>
-      <small> ISP </small>
-      <h3>${ip.isp}</h3>
- 
+  <div>
+  <small> IP ADDRESS </small>
+  <h3>${ip.ip}</h3>
+</div>
+<div class="divider">
+<small> LOCATION </small>
+<h3>${ip.location.region}, ${ip.location.country} ${ip.location.postalCode}</h3>
+</div>
+<div class="divider">
+<small> TIMEZONE </small>
+<h3>${timeZone} ${ip.location.timezone}</h3>
+</div>
+<div class="divider">
+<small> ISP </small>
+<h3>${ip.isp}</h3>
+</div>
 `;
 
   ipContainer.innerHTML = html;
 };
 
 // Display Map
+var customIcon = L.icon({
+  iconUrl: "images/icon-location.svg",
+  iconSize: [50, 60],
+  iconAnchor: [19, 5],
+  popupAnchor: [0, -38],
+});
+
 const renderMap = () => {
-  var customIcon = L.icon({
-    iconUrl: "images/icon-location.svg",
-    iconSize: [50, 60],
-    iconAnchor: [19, 5],
-    popupAnchor: [0, -38],
-  });
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       var map = L.map("map", {
         zoomControl: false,
-        scrollWheelZoom: "center",
-        dragging: false,
+        // scrollWheelZoom: "center",
+        // dragging: false,
       }).setView([latitude, longitude], 15);
 
-      L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+      L.tileLayer(
+        "https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+        {}
+      ).addTo(map);
 
       var marker = L.marker([latitude, longitude], { icon: customIcon }).addTo(
         map
@@ -57,7 +65,7 @@ const renderMap = () => {
   );
 };
 
-// Get IP infos on page load
+// Get IP data on page load
 const getIPData = async () => {
   try {
     const res1 = fetch(
@@ -85,20 +93,26 @@ const getIPData = async () => {
 };
 getIPData();
 
-// Submit/Fetch IP infos
-submitBtn.addEventListener("click", function () {
-  const fetchIP = async function (ip) {
+// Search for IP address
+const fetchIP = async function (ip) {
+  try {
     const res = await fetch(
       `https://geo.ipify.org/api/v2/country,city?apiKey=at_4HJeO7HnHJvDPvyC2W5SMVFqqgEMU&ipAddress=${ip}`
     );
     const ipData = await res.json();
     const timeZone = await getIPData();
     renderIPData(ipData, timeZone);
-  };
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+searchBtn.addEventListener("click", function () {
   fetchIP(inputEl.value);
 });
 
 // Prevent Default on input
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  fetchIP(inputEl.value);
 });
